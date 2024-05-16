81fbcc16c01bbc3a23d87047928685d4c2971931
Caio Costa <caiosouzacosta@santos.sp.gov.br>
2024-05-16 12:04:04 -0300

    Sanctum config, AuthResource/Controller and working login

---
+++ backend/app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\Auth\AuthResource;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request) {     
+        $user = User::where('email', $request->email)->first();
+     
+        if (!$user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+     
+        $token = $user->createToken('myToken')->plainTextToken;
+
+        return AuthResource::make([
+            'token' => $token,
+            'user' => [
+                'name' => $user->name,
+                'email' => $user->email
+            ],
+        ]);
+    }
+
+    public function register() {
+
+    }
+}
