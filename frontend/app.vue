5964dacc7215b810118b007dff883986e4f1ec79
Caio Costa <caiosouzacosta@santos.sp.gov.br>
2024-05-17 11:20:21 -0300

    Meta tags

--- frontend/app.vue
+++ frontend/app.vue
@@ -3,3 +3,28 @@
     <NuxtPage></NuxtPage>
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk
+      ? `${titleChunk} - FormLilly`
+      : "FormLilly";
+  },
+  meta: [
+    {
+      name: "description",
+      content: "Efficient form builder with Nuxt 3 and Laravel 10, supported by robust authentication provided by Laravel Sanctum.",
+    },
+    {
+      name: "keywords",
+      content:
+        "Form, Question, Answer, Fullstack Developer, Back-end, Front-end, Hard Skills, Soft Skills, HTML, CSS, JavaScript, TypeScript, Vue.js, Nuxt.js, PHP, Laravel, MySQL, Microsoft Windows, Linux",
+    },
+    {
+      name: "autor",
+      content: "Caio Costa",
+    },
+  ],
+});
+</script>
